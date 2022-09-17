70c6b4f584667c06890da0c313a8212558968c6f
SoyaNyan <nyan@soya.moe>
2022-09-17 22:11:13 +0900

    removed unused utils

--- papi/typescripts/enchantScrollCore.ts
+++ papi/typescripts/enchantScrollCore.ts
@@ -13,9 +13,7 @@
 /**
   [ global objects ] 
 */
-const Data: any = new Object()
 const PlaceholderAPI: any = new Object()
-const Placeholder: any = new Object()
 const BukkitServer: any = new Object()
 const BukkitPlayer: any = new Object()
 const args: string[] = []
@@ -1068,90 +1066,6 @@ const TITLE_SETTINGS: {
 }
 
 /**
-  [ data utilities ] 
-*/
-// check if data(key) exists in global store
-function exists(path: string): boolean {
-	// returns true if a key exists; else false. (Placeholder API)
-	return Data.exists(path)
-}
-
-// get data from global store
-function get(path: string): DataType {
-	let result: DataType
-
-	// check data exists
-	if (path.length > 0) {
-		// check data exists
-		if (!exists(path)) return false
-
-		// returns the value stored under key. (Placeholder API)
-		// type of data value => number | string | boolean
-		result = Data.get(path)
-	} else {
-		// returns a Map<String, Object> of the entire placeholder script's data. (Placeholder API)
-		result = Data.getData()
-	}
-
-	return result
-}
-
-// set data in global store
-function set(path: string, payload: DataType): boolean {
-	// check data exists
-	if (exists(path)) {
-		return update(path, payload)
-	}
-
-	// stores a value under key. (Placeholder API)
-	Data.set(path, payload)
-	save()
-
-	return true
-}
-
-// update data in global store
-function update(path: string, payload: DataType): boolean {
-	// check data exists
-	if (!exists(path)) {
-		return set(path, payload)
-	}
-
-	// stores a value under key. (Placeholder API)
-	Data.set(path, payload)
-	save()
-
-	return true
-}
-
-// remove data from global store
-function remove(path: string): boolean {
-	// check data exists
-	if (!exists(path)) return false
-
-	// removes a key from the data. (Placeholder API)
-	Data.remove(path)
-	save()
-
-	return true
-}
-
-// remove all data from global store
-function clear(): boolean {
-	// removes all data. (Placeholder API)
-	Data.clear()
-	save()
-
-	return true
-}
-
-// saves current state
-function save(): void {
-	// saves the current data state to the data file. (Placeholder API)
-	Placeholder.saveData()
-}
-
-/**
   [ general utilities ] 
 */
 // stringify data for placeholder return
@@ -1384,6 +1298,7 @@ function getVersion(): number {
 	if (version.includes('1.18')) return 18
 	if (version.includes('1.19')) return 19
 	if (version.includes('1.19.1')) return 19.1
+	if (version.includes('1.19.2')) return 19.2
 
 	// unknown(unsupported) version
 	return -1
